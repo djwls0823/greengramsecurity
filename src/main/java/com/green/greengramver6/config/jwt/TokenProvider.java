665487efd862f577a2df80bbec59ccb1cef99313
Lee eo jin <djwls7777@gmail.com>
2024-12-16 17:36:49 +0900

    security

---
+++ src/main/java/com/green/greengramver6/config/jwt/TokenProvider.java
@@ -0,0 +1,88 @@
+package com.green.greengramver6.config.jwt;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.green.greengramver6.config.security.MyUserDetails;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.util.Date;
+
+@Service
+public class TokenProvider {
+    private final ObjectMapper objectMapper; //Jackson 라이브러리
+    private final JwtProperties jwtProperties;
+    private final SecretKey secretKey;
+
+    public TokenProvider(ObjectMapper objectMapper, JwtProperties jwtProperties) {
+        this.objectMapper = objectMapper;
+        this.jwtProperties = jwtProperties;
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtProperties.getSecretKey()));
+    }
+
+    // JWT 생성
+    public String generateToken(JwtUser jwtUser, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(jwtUser, new Date(now.getTime() + expiredAt.toMillis()));
+    }
+
+    private String makeToken(JwtUser jwtUser, Date expiry) {
+        return Jwts.builder()
+                .header().add("typ", "JWT")
+                         .add("alg", "HS256")
+                .and()
+                .issuer(jwtProperties.getIssuer())
+                .issuedAt(new Date())
+                .expiration(expiry)
+                .claim("signedUser", makeClaimByUserToString(jwtUser))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    private String makeClaimByUserToString(JwtUser jwtUser) {
+        //객체 자체를 JWT에 담고 싶어서 객체를 직렬화
+        //jwtUser에 담고 있는 데이터를 문자열 형태인 JSON형태의 문자열로 변환
+        try {
+           return objectMapper.writeValueAsString(jwtUser);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean validToken(String token) {
+        try {
+            //JWT 복호화
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = getUserDetailsFromToken(token);
+        return userDetails == null
+                ? null
+                : new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    public UserDetails getUserDetailsFromToken(String token) {
+        Claims claims = getClaims(token);
+        String json = (String)claims.get("signedUser");
+        JwtUser jwtUser = objectMapper.convertValue(json, JwtUser.class);
+        MyUserDetails userDetails = new MyUserDetails();
+        userDetails.setJwtUser(jwtUser);
+        return userDetails;
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+    }
+}
